bdb281be372da439bb9b7369e01d2bcd3864bc27
Your NameKhetam_He <toty.f.h@hotmail.com>
2019-04-30 12:38:43 +0300

    add multiple images

--- js/newindex.js
+++ js/newindex.js
@@ -5,42 +5,89 @@ var ctx = canvas.getContext('2d');
 var img = new Image();
 img.crossOrigin="anonymous";
 
-window.addEventListener('load', DrawPlaceholder);
+window.addEventListener('load', DrawPlaceholder(img,ctx,"#A89172",'11.jpg',170,210));
+
 
 var canvas1 = document.getElementById('imageCanvas1');
 var ctx1 = canvas1.getContext('2d');
 var img1 = new Image();
 img1.crossOrigin="anonymous";
 
-window.addEventListener('load', DrawPlaceholder1);
+window.addEventListener('load', DrawPlaceholder(img1,ctx1,"#D3CFC8",'33.jpg',175,260));
 
-function DrawPlaceholder() {
-    img.onload = function() {
-        DrawOverlay(img);
-        DrawText();
-        DynamicText(img)
+
+var canvas2 = document.getElementById('imageCanvas2');
+var ctx2 = canvas2.getContext('2d');
+var img2 = new Image();
+img1.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder(img2,ctx2,"#E6E0DB",'44.jpg',90,300));
+
+
+var canvas3 = document.getElementById('imageCanvas3');
+var ctx3 = canvas3.getContext('2d');
+var img3 = new Image();
+img1.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder(img3,ctx3,"#7E9E85",'55.jpg',250,180));
+
+
+
+var canvas4 = document.getElementById('imageCanvas4');
+var ctx4 = canvas4.getContext('2d');
+var img4 = new Image();
+img1.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder(img4,ctx4,"#7E9E85",'66.jpg',250,170));
+
+
+
+var canvas5 = document.getElementById('imageCanvas5');
+var ctx5 = canvas5.getContext('2d');
+var img5 = new Image();
+img1.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder(img5,ctx5,"#A89172",'11.jpg',170,210));
+
+
+
+var canvas6 = document.getElementById('imageCanvas6');
+var ctx6 = canvas6.getContext('2d');
+var img6 = new Image();
+img1.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder(img6,ctx6,"#D3CFC8",'33.jpg',175,260));
+
+
+
+
+function DrawPlaceholder(photo,canv,clr,sorc,x,y) {
+    photo.onload = function() {
+        DrawOverlay(photo,canv);
+        DrawText(canv,clr);
+        DynamicText(canv,photo,clr,x,y)
 };
-    img.src = '2.jpg';
+    photo.src =sorc;
   
 }
-function DrawOverlay(img) {
-ctx.drawImage(img,0,0);
-    ctx.fillStyle = 'rgba(0, 0, 0, 0)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+function DrawOverlay(photo,canv) {
+canv.drawImage(photo,0,0);
+    canv.fillStyle = 'rgba(0, 0, 0, 0)';
+    canv.fillRect(0, 0, canvas.width, canvas.height);
 }
-function DrawText() {
-    ctx.fillStyle = "#fbf2eb"; 
-    ctx.textBaseline = 'middle';
-    ctx.font = "20px 'Montserrat'";
+function DrawText(canv,fillcolor) {
+    canv.fillStyle = fillcolor; 
+    canv.textBaseline = 'middle';
+    canv.font = "25px 'Montserrat'";
 }
-function DynamicText(img) {
+function DynamicText(canv,photo,clr,x,y) {
   document.getElementById('name').addEventListener('keyup', function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    DrawOverlay(img);
-    DrawText(); 
+    canv.clearRect(0, 0, canvas.width, canvas.height);
+    DrawOverlay(photo,canv);
+    DrawText(canv,clr); 
     text_title = this.value;
-    ctx.fillText(text_title, 170, 250);
-    ctx.textAlign = 'center';
+    canv.fillText(text_title, x, y);
+    canv.textAlign = 'center';
 
   });
 }
@@ -58,8 +105,8 @@ function handleImage(e) {
     img.src = event.target.result;
         src = event.target.result;
         canvas.classList.add("show");
-        DrawOverlay(img);
-        DrawText(); 
+        DrawOverlay(photo,canv);
+        DrawText(ctx,"#567837"); 
         DynamicText(img);   
     }
 
@@ -72,62 +119,11 @@ download_img = function(el) {
   el.href = image;
 };
 
-function DrawPlaceholder1() {
-    img1.onload = function() {
-        DrawOverlay1(img1);
-        DrawText1();
-        DynamicText1(img1)
-};
-    img1.src = '1.jpg';
-  
-}
-function DrawOverlay1(img1) {
-ctx1.drawImage(img1,0,0);
-    ctx1.fillStyle = 'rgba(0, 0, 0, 0)';
-    ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
-}
-function DrawText1() {
-    ctx1.fillStyle = "#7E9E86"; 
-    ctx1.textBaseline = 'middle';
- 
-    ctx1.font = "20px 'Montserrat'";
-}
-function DynamicText1(img1) {
-  document.getElementById('name').addEventListener('keyup', function() {
-    ctx1.clearRect(0, 0, canvas.width, canvas.height);
-    DrawOverlay1(img1);
-    DrawText1(); 
-    text_title = this.value;
-    ctx1.fillText(text_title, 250, 180);
-    ctx1.textAlign = 'center';
-
-  });
-}
-function handleImage1(e) {
-    var reader1 = new FileReader();
-    var img1 = "";
-    var src1 = "";
-    reader1.onload = function(event) {
-        img1 = new Image();
-        img1.onload = function() {
-            canvas1.width = img1.width;
-            canvas1.height = img1.height;
-            ctx1.drawImage(img1,0,0);
-        }
-    img1.src = event.target.result;
-        src1 = event.target.result;
-        canvas1.classList.add("show");
-        DrawOverlay1(img1);
-        DrawText1(); 
-        DynamicText1(img1);   
-    }
-
-    reader.readAsDataURL(e.target.files[0]); 
- 
-}
 
 
-download_img1 = function(el1) {
+
+
+g1 = function(el1) {
   var image1 = canvas1.toDataURL("image/jpg");
   el1.href = image1;
 };
